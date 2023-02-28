b32abc411839b44ca9d0eb1c61dd17234e85603f
Jakob Edelstein <jakobedelstein@gmail.com>
2023-02-28 08:34:52 -0800

    create audioContext before rendering audio modules

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,26 @@ import OscillatorSimple from '../components/OscillatorSimple.vue'
 
 <template>
 	<main>
-		<OscillatorSimple />
+		<button @click="createAudioContext">Create Audio Context</button>
+		<div v-if="audioContextCreated">
+			<OscillatorSimple :audioContext="audioContext"/>
+		</div>
 	</main>
 </template>
+
+<script>
+	export default {
+		data () {
+			return {
+				audioContext: null,
+				audioContextCreated: false
+			}
+		},
+		methods: {
+			createAudioContext () {
+				this.audioContext = new AudioContext()
+				this.audioContextCreated = true
+			}
+		}
+	}
+</script>
